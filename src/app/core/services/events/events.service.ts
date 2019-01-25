bc3265ac49d2775f3577fa3c7067ed167d00f285
Troy Hicks <NOLAWolfe@users.noreply.github.com>
2019-01-25 09:10:57 -0500

    pushing updated services, need to get updated models for progress

---
+++ src/app/core/services/events/events.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  httpHeaders = new HttpHeaders({
+    'Content-Type': 'application.json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  });
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllEvents() {
+    return this.httpClient.get<Event[]>('http://localhost:8083/');
+  }
+
+  addEvent(event: Event) {
+    return this.httpClient.post(`http://localhost:8083`, event);
+  }
+  // updateEvent(event: Event) {
+  //   return this.httpClient.put(`http://localhost:8080`), {'id': id, 'Created': Date};
+  // }
+
+  getEventById(id: number) {
+    return this.httpClient.get<Event[]>(`http://localhost:8083`);
+  }
+  getEventsByCategory(categoryID: number) {
+    return this.httpClient.get<Event[]>(`http://localhost:8080/${categoryID}`);
+  }
+  getEventsByUderId(userid: number) {
+    return this.httpClient.get<Event[]>(`http://localhost:8080/${userid}`);
+  }
+
+  registerForEvent(eventId: number, userId: number) {
+    return this.httpClient.post('', eventId);
+  }
+}
---
+++ src/app/core/services/users/users.service.ts
@@ -0,0 +1,32 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UsersService {
+//   httpHeaders = new HttpHeaders({
+//     'Content-Type': 'application.json',
+//     'Accept': 'application/json',
+//     'Access-Control-Allow-Headers': 'Content-Type'
+//   });
+//   constructor(private httpClient: HttpClient) { }
+
+//   getAllUsers() {
+//     return this.httpClient.get<User[]>('http://localhost:8083/');
+//   }
+
+//   addUser(user: User) {
+//     return this.httpClient.post(`http://localhost:8083`, user);
+//   }
+//   // updateEvent(event: Event) {
+//   //   return this.httpClient.put(`http://localhost:8080`), {'id': id, 'Created': Date};
+//   // }
+
+
+//   getUserById(id: number) {
+//     return this.httpClient.get<User[]>(`http://localhost:8083`);
+//   }
+//   getUserByEventId(commentId: number) {
+//     return this.httpClient.get<User[]>(`http://localhost:8080/${commentId}`);
+//   }
+// }
