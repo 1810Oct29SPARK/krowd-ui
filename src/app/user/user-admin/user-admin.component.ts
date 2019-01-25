0ad7740f2a2a771aadc71e92bfb4accaa01063ab
Elias Vigil <eli.vigil718@gmail.com>
2019-01-25 11:06:03 -0500

    initial admin stuff

--- src/app/user/user-admin/user-admin.component.ts
+++ src/app/user/user-admin/user-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-user-admin',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+ 
 
   ngOnInit() {
   }
 
+  showModal() {
+    
+  }
 }
