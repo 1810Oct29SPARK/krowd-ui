098d0958c0dd5778072628be3e86434a798298da
sean sulham <sean.sulham@hotmail.com>
2019-01-24 21:57:20 -0500

    Updated user-async.component.ts

--- src/app/user-async/user-async.component.ts
+++ src/app/user-async/user-async.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAsyncService } from './user-async.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-async',
   templateUrl: './user-async.component.html',
-  styleUrls: ['./user-async.component.css']
+  styleUrls: ['./user-async.component.css'],
+  providers: [UserAsyncService]
 })
 export class UserAsyncComponent implements OnInit {
+  isLoggedIn = false;
+  user: { name: string };
+  userDetail;
+  systemError = false;
+  systemErrorMessage = '';
 
-  constructor() { }
+  constructor(private userAsyncService: UserAsyncService) { }
 
   ngOnInit() {
+    this.userAsyncService = this.userAsyncService.getUserDetails().subscribe(
+      (response: string) => {
+        this.userDetail = response;
+        this.isLoggedIn = true;
+      },
+      (error: string) => {
+        this.systemError = true;
+        this.systemErrorMessage = error;
+      }
+    )
   }
 
 }
