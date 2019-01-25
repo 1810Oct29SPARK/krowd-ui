15771ab962ce04f4017f62da6800dd2bdf009825
sean sulham <sean.sulham@hotmail.com>
2019-01-25 09:21:50 -0500

    Fixed error '**' to '&&'

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service'
+import { UserService } from './user.service';
 
 import { UserComponent } from './user.component';
 
