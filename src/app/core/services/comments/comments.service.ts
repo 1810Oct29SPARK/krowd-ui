fa55117b17aca185aa7f377e490cd38983fea166
Kaleigh Jaeger <jaegerka@dukes.jmu.edu>
2019-01-29 13:43:39 -0500

    Deleted not-needed components and added http service

--- src/app/core/services/comments/comments.service.ts
+++ src/app/core/services/comments/comments.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Comment } from '../../../shared/models/comment';
+import { HttpService } from '../../services/http/http.service'
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/from';
@@ -12,7 +13,7 @@ import 'rxjs/add/operator/catch';
 })
 export class CommentsService {
   private comments: Comment[] = [];
-
+  
   httpHeaders = new HttpHeaders({
     'Content-Type': 'application.json',
     'Accept': 'application/json',
@@ -21,27 +22,28 @@ export class CommentsService {
 
   constructor(private httpClient: HttpClient) { }
 
+
   getAllComments() {
-    return this.httpClient.get<Event[]>('http://localhost:8083/');
+    return this.httpClient.get<Event[]>(HttpService.baseUrl);
   }
 
   addComment(comment: Comment) {
-    return this.httpClient.post(`http://localhost:8083`, comment);
+    return this.httpClient.post(HttpService.baseUrl, comment);
   }
   // updateEvent(event: Event) {
   //   return this.httpClient.put(`http://localhost:8080`), {'id': id, 'Created': Date};
   // }
 
   getCommentById(id: number) {
-    return this.httpClient.get<Comment[]>(`http://localhost:8083`);
+    return this.httpClient.get<Comment[]>(HttpService.baseUrl);
   }
 
   getCommentsByUserId(userId: number) {
-    return this.httpClient.get<Comment[]>(`http://localhost:8080/${userId}`);
+    return this.httpClient.get<Comment[]>(HttpService.baseUrl + `${userId}`);
   }
 
   getCommentByEventId(commentId: number) {
-    return this.httpClient.get<Comment[]>(`http://localhost:8080/${commentId}`);
+    return this.httpClient.get<Comment[]>(HttpService.baseUrl+ `${commentId}`);
   }
 
 }
