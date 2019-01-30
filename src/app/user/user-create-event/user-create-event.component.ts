6ee066b2067598830f14493a6fedb031e5508ac4
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-29 22:09:53 -0500

    can now add an event

--- src/app/user/user-create-event/user-create-event.component.ts
+++ src/app/user/user-create-event/user-create-event.component.ts
@@ -23,9 +23,11 @@ export class UserCreateEventComponent implements OnInit {
   eventDescription: any = null;
   eventDate: any = null;
   eventTime: any = null;
+  eventApartment: any = null;
+  userId: any = null;
 
   // code for image upload
-  selectedFile: File = null;
+  eventPhotoID: File = null;
   imageURL: string;
   picture = 'http://saveabandonedbabies.org/wp-content/uploads/2015/08/default.png';
 
@@ -40,37 +42,53 @@ export class UserCreateEventComponent implements OnInit {
   ngOnInit() {
   }
 
-  onEventCreated(form: NgForm) {
-    console.log('Event Successfully Created');
-    console.log(form);
-    const value = form.value;
-    console.log(form.value);
+  createEvent(form: NgForm) {
+  this.http.post('http://localhost:8085/event/add', {
+  'eventName': this.eventName,
+  'eventCategory': this.assignCategory(this.eventCategory),
+  'eventDate': JSON.stringify(this.eventDate),
+  'eventAddress': this.eventAddress,
+  'eventApartment': this.eventApartment,
+  'eventCity': this.eventCity,
+  'eventState': this.eventState,
+  'eventZip': this.eventZip,
+  'eventDescription': this.eventDescription,
+  'eventFlag': JSON.stringify(0),
+  'userID': 1,
+  'eventPhotoID': ''
+}).subscribe((result) => {
+});
+    // console.log('Event Successfully Created');
+    // console.log(form);
+    // const value = form.value;
+    // console.log(form.value);
 
-    const userId = 0;
-    // this.authService.getCurrentUser();
+    // const userId = 0;
+    // // this.authService.getCurrentUser();
 
-    const eventCategory = this.assignCategory(value.eventCategory);
+    // const eventCategory = this.assignCategory(value.eventCategory);
 
 
-    const newEvent = new Event(
-      null,
-      value.eventName,
-      eventCategory,
-      value.eventDate,
-      value.eventTime,
-      value.eventAddress,
-      value.eventApartment,
-      value.eventCity,
-      value.eventState,
-      value.eventZip,
-      null,
-      0,
-      userId,
-      this.imageURL
-    );
-    console.log(newEvent);
+    // const newEvent = new Event(
+    //   null,
+    //   value.eventName,
+    //   eventCategory,
+    //   value.eventDate,
+    //   value.eventTime,
+    //   value.eventAddress,
+    //   value.eventApartment,
+    //   value.eventCity,
+    //   value.eventState,
+    //   value.eventZip,
+    //   null,
+    //   0,
+    //   userId,
+    //   this.imageURL
+    // );
     this.submitted = true;
-    this.eventservice.addEvent(newEvent);
+    // this.eventservice.addEvent(value.eventName, value.eventCategory, value.eventDate, value.eventTime,
+    //   value.eventAddress, value.eventApartment, value.eventCity, value.eventState, value.eventZip, null,
+    //   0, value.userId, this.eventPhotoID);
   }
 
   assignCategory(category) {
@@ -78,7 +96,7 @@ export class UserCreateEventComponent implements OnInit {
       case 'Art': {
         return 1;
       }
-      case 'Food & Dring': {
+      case 'Food & Drink': {
         return 2;
       }
       case 'Music': {
@@ -92,6 +110,9 @@ export class UserCreateEventComponent implements OnInit {
       }
       case 'Volunteering': {
         return 6;
+      }
+      case 'Other': {
+        return 7;
       }
     }
   }
@@ -113,3 +134,20 @@ export class UserCreateEventComponent implements OnInit {
   }
 
 }
+
+// this.http.post('http://localhost:8085/event/add', {
+//   'eventName': this.value.eventName,
+//   'eventCategory': this.value.eventCategory,
+//   'eventDate': this.value.eventDate,
+//   'eventAddress': this.value.eventAddress,
+//   'eventApartment': this.value.eventApartment,
+//   'eventCity': this.value.eventCity,
+//   'eventState': this.value.eventState,
+//   'eventZip': this.value.eventZip,
+//   'eventDescription': this.value.eventDescription,
+//   'eventFlag': JSON.stringify(0),
+//   'userID': this.value.userId,
+//   'eventPhotoID': this.eventPhotoID
+// }).subscribe((result) => {
+
+// });
