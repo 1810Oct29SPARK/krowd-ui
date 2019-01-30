9c70ae932fc0a4dc33d2f26b4e9f7fc4d1c2fe30
jerbjer <jerbjer@gmail.com>
2019-01-30 16:24:29 -0500

    adding auth guards

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {
     path: 'user',
     loadChildren: '../user/user.module#UserModule',
-    // canActivate: [AuthGaurdService]
+    canActivate: [AuthGaurdService]
   },
   {
     path: 'landing', component: LandingComponent,
