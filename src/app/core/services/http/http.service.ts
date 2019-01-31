70d89712d1c3006f16f7724fc9a7bbf98d4ae03b
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-31 00:25:36 -0500

    switched to h2 for testing purposes, will change back when creating an event works

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -7,7 +7,8 @@ export class HttpService {
 
   constructor() { }
 
-  public static baseUrl = 'http://ec2-3-91-103-185.compute-1.amazonaws.com:8080/krowd-boot/';
+  // public static baseUrl = 'http://ec2-3-91-103-185.compute-1.amazonaws.com:8080/krowd-boot/';
+  public static baseUrl = 'http://localhost:8085/';
 
 
 
